c1abdc7fca608bf079161e2f0f347da1da6e4474
ProfSammy <87666814+ProfSammy@users.noreply.github.com>
2022-10-07 16:17:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let ground;
+let lander;
+var lander_img;
+var bg_img;
+
+
+var vx = 0;
+var g = 0.05;
+var vy = 0;
+
+function preload()
+{
+  
+}
+
+function setup() {
+  createCanvas(1000,700);
+  frameRate(80);
+
+ 
+}
+
+function draw() 
+{
+  background(51);
+  
+}
+
+
